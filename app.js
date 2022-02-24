f57c9dd250db8a6c78a9f488366d225040f26c4f
bijoy <uzumakibijoy2017@gmail.com>
2022-02-24 21:50:13 +0600

    tip calculator at it's first stage

---
+++ app.js
@@ -0,0 +1,26 @@
+document.querySelector(".form").onchange =function () {
+
+const totalbill= Number(document.querySelector("#total-bill").value) ;
+const tip=document.querySelector("#tipInput").value;
+const tipOutPut=document.querySelector("#tipOutPut");
+const tipAmount=document.querySelector("#tipAmount");
+const TotalBillWithTip=document.querySelector("#TotalBillWithTip");
+const results=document.querySelector(".results");
+
+
+tipOutPut.innerHTML=`${tip}%`
+results.style.display="block"
+
+const tipvalue = totalbill * (tip/100);
+tipAmount.value=tipvalue.toFixed(2)
+
+const billWithTip = tipvalue + tip;
+TotalBillWithTip.value=Math.ceil(billWithTip)
+// TotalBillWithTip.value=billWithTip.toFixed(2)
+
+
+
+
+    tipAmount.innerHTML=`<p>${tipvalue}</p>`
+}
+
